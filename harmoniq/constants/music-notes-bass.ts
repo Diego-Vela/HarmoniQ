2a5ed8103abfe1c5e008f74f27fb61b2ba9ebae1
Diego Vela <Diegov5498@gmail.com>
2025-04-21 19:21:12 -0700

    Adding Bass Clef images

--- harmoniq/constants/music-notes-bass.ts
+++ harmoniq/constants/music-notes-bass.ts
@@ -14,9 +14,33 @@ import a3 from "@/assets/images/music/no-time-sig/bass/a3.png";
 import b3 from "@/assets/images/music/no-time-sig/bass/b3.png";
 import c4 from "@/assets/images/music/no-time-sig/bass/c4.png";
 
+import csharp2 from "@/assets/images/music/no-time-sig/bass/csharp2.png";
+import dsharp2 from "@/assets/images/music/no-time-sig/bass/dsharp2.png";
+import fsharp2 from "@/assets/images/music/no-time-sig/bass/fsharp2.png";
+import gsharp2 from "@/assets/images/music/no-time-sig/bass/gsharp2.png";
+import asharp2 from "@/assets/images/music/no-time-sig/bass/asharp2.png";
+import csharp3 from "@/assets/images/music/no-time-sig/bass/csharp3.png";
+import dsharp3 from "@/assets/images/music/no-time-sig/bass/dsharp3.png";
+import fsharp3 from "@/assets/images/music/no-time-sig/bass/fsharp3.png";
+import gsharp3 from "@/assets/images/music/no-time-sig/bass/gsharp3.png";
+import asharp3 from "@/assets/images/music/no-time-sig/bass/asharp3.png";
+
+import dflat2 from "@/assets/images/music/no-time-sig/bass/dflat2.png";
+import eflat2 from "@/assets/images/music/no-time-sig/bass/eflat2.png";
+import gflat2 from "@/assets/images/music/no-time-sig/bass/gflat2.png";
+import aflat2 from "@/assets/images/music/no-time-sig/bass/aflat2.png";
+import bflat2 from "@/assets/images/music/no-time-sig/bass/bflat2.png";
+import dflat3 from "@/assets/images/music/no-time-sig/bass/dflat3.png";
+import eflat3 from "@/assets/images/music/no-time-sig/bass/eflat3.png";
+import gflat3 from "@/assets/images/music/no-time-sig/bass/gflat3.png";
+import aflat3 from "@/assets/images/music/no-time-sig/bass/aflat3.png";
+import bflat3 from "@/assets/images/music/no-time-sig/bass/bflat3.png";
+
 import base from "@/assets/images/music/time-sig/rythm/rythm-empty.png";
 
 export const bassClefNotes = {
+  c2,
+  d2,
   e2,
   f2,
   g2,
@@ -30,6 +54,30 @@ export const bassClefNotes = {
   a3,
   b3,
   c4,
+
+  // Sharps
+  csharp2,
+  dsharp2,
+  fsharp2,
+  gsharp2,
+  asharp2,
+  csharp3,
+  dsharp3,
+  fsharp3,
+  gsharp3,
+  asharp3,
+
+  // Flats
+  dflat2,
+  eflat2,
+  gflat2,
+  aflat2,
+  bflat2,
+  dflat3,
+  eflat3,
+  gflat3,
+  aflat3,
+  bflat3,
 }
 
 export const rythmNotes = {
